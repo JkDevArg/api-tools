946b3a8b9b1af76d2546db3b6d17e00fa4e217f9
Joaquin Centurion <jcenturion@focusit.pe>
2024-01-29 10:12:19 -0500

    change logger on mail, add env.example endpoint api url scan

--- src/mail/dto/mail.dto.ts
+++ src/mail/dto/mail.dto.ts
@@ -12,7 +12,7 @@ export class sendEmailDto {
     @IsString()
     @IsOptional()
     body?: string;
-    
+
     @IsString()
     @IsOptional()
     from?: string;
--- src/urlscan/urlscan.service.ts
+++ src/urlscan/urlscan.service.ts
@@ -5,7 +5,7 @@ import { UserActiveInterface } from 'src/common/interfaces/user-active.interface
 import { axiosErrorHandler } from 'src/common/utils/http-resp.utils';
 import { Repository } from 'typeorm';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Cache } from 'cache-manager';  
+import { Cache } from 'cache-manager';
 import { Role } from 'src/common/enums/rol.enum';
 import { URLSCAN } from './entities/urlscan.entity';
 import { getUrlScanDto } from './dto/urlscan.dto';
